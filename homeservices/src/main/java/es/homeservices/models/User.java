e59ba8b5f39de3feee760d2d9d9a84681d4e1096
Pedro Rafael <pedro.damiao@ccc.ufcg.edu.br>
2022-12-06 15:58:37 -0300

    iniciado classe usuário

---
+++ homeservices/src/main/java/es/homeservices/models/User.java
@@ -0,0 +1,69 @@
+package main.java.es.homeservices.models;
+
+import java.util.Date;
+
+public class User {
+
+    private String name;
+    private String cpf;
+    private Date birthDate;
+    private String email;
+    private String cel;
+    /*private Location res;*/
+    private String pswd;
+
+    public User(String name, String cpf, String email, String pswd) {
+        this.name = name;
+        this.cpf = cpf;
+        this.email = email;
+        this.pswd = pswd;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCel() {
+        return cel;
+    }
+
+    public void setCel(String cel) {
+        this.cel = cel;
+    }
+
+    public String getPswd() {
+        return pswd;
+    }
+
+    public void setPswd(String pswd) {
+        this.pswd = pswd;
+    }
+}
